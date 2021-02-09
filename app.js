ac36abf0635eae1ecab60870416ce4b44eb74991
krishna rokhale <krishna.rokhale@gmail.com>
2021-02-08 17:27:42 -0800

    cheking in

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
 
 var app = express();
+
+app.use(cors())
 
 // instances and classes - Object oriented programming in javascript
 
--- routes/dogs.js
+++ routes/dogs.js
@@ -3,20 +3,42 @@ var router = express.Router();
 const {Dog} = require('../lib/models');
 
 // CREATE A RESTful API for the DOG model/entity
+// Users
+// Cars
 
 // CRUD - Create, Read, Update, Delete
 
-// POST /dogs
-// GET /dogs/2
-// PUT /dogs/3
-// DELETE /dogs/4
+// POST /dogs - Create
+// GET /dogs/2 - Fetching/reading
+// PUT /dogs/3 - Updating the dog with id = 2
+// DELETE /dogs/7 - Delete the dog with id = 4
+
+router.delete('/:id', async function(req, res, next) {
+    console.log(req.params)
+    // ask the DB to delete the record with id = req.params.id
+    let dog = await Dog.destroy({where: {id: req.params.id}});
+
+    res.json({});
+});
+
+router.get('/', async function(req, res, next) {
+    console.log('GET request for all dogs in the system received')
+    let dogs = await Dog.findAll({});
+    // let dog = await Dog.create(req.body);
+    // let dog = await Dog.create({firstName: req.body.firstName, lastName: req.body.lastName});
+    // console.log(dog)
+    res.json(dogs);
+});
 
 router.post('/', async function(req, res, next) {
     console.log(req.body);
-    var dog = await Dog.create(req.body);
-    Dog.create()
+    // next up is savind the info in the DB and sending back a response to the react server.
+    let dog = await Dog.create(req.body);
+
+
     // let dog = await Dog.create({firstName: req.body.firstName, lastName: req.body.lastName});
-    console.log(dog)
+    // console.log(dog)
+    // res.json({});
     res.json(dog);
 });
 
